a5a792f1e71eb378d205e7fba5148819ee6191b0
Tran Manh Cuong <cunbeo04012017@gmail.com>
2023-07-17 12:55:27 +0700

    Final Project

---
+++ api/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connect to database.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnecter", () => {
+  console.log("DB disconnected");
+});
+mongoose.connection.on("connecter", () => {
+  console.log("DB connected");
+});
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err,req,res,next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Bir hata oluştu.";
+    return res.status(errorStatus).json({
+      message: errorMessage,
+      status: errorStatus,
+      success: false,
+      stack: err.stack,
+    });
+  })
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connect to backend.");
+});
